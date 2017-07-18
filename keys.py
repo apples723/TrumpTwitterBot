08f6ce7f7115248f0d71e24362066a961837bd70
Siders Grant <gsiders@danfoss.com>
2017-07-18 16:21:21 -0500

    file creation

---
+++ keys.py
@@ -0,0 +1,6 @@
+keys = dict(
+	consumer_key =          'mRoKmd9Rqacq0OTBCI3v9z60Y',
+	consumer_secret =       '8qHUB21487SVdTlBub84lrE8YKgRwQCznCLJYBszzO6dfzlIhA',
+	access_token =          '2600560549-1WggRlxJkxsr9KaVCjqmxABehfVqZbunnjR2m4z',
+	access_token_secret =   'yOeV4C7amYDcYxEwkYYbAGSfhSJsmaULvcL7XMgRNAQIn',
+)
---
+++ tweet.py
@@ -0,0 +1,18 @@
+import tweepy
+#from our keys module (keys.py), import the keys dictionary
+from keys import keys
+ 
+CONSUMER_KEY = keys['consumer_key']
+CONSUMER_SECRET = keys['consumer_secret']
+ACCESS_TOKEN = keys['access_token']
+ACCESS_TOKEN_SECRET = keys['access_token_secret']
+ 
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+
+
+stuff = api.user_timeline(screen_name = 'realDonaldTrump', count = 100, include_rts = True)   
+ 
+
