ec77149e0fe60cf304783390b05e3821dcd1edd8
Siders Grant <gsiders@danfoss.com>
2017-07-19 10:42:08 -0500

    some changes auto added by update script.bat at Wed 07/19/2017 10:42:05.53

--- keys.py
+++ keys.py
@@ -1,6 +1,14 @@
+#So my private twitter auth keys aren't on git.....
+AllKeys = open('..\keys.txt', 'r')
+lines = []
+for line in AllKeys:	
+	lines.append(line)
+AllKeys.close 
+
 keys = dict(
-	consumer_key =          'mRoKmd9Rqacq0OTBCI3v9z60Y',
-	consumer_secret =       '8qHUB21487SVdTlBub84lrE8YKgRwQCznCLJYBszzO6dfzlIhA',
-	access_token =          '2600560549-1WggRlxJkxsr9KaVCjqmxABehfVqZbunnjR2m4z',
-	access_token_secret =   'yOeV4C7amYDcYxEwkYYbAGSfhSJsmaULvcL7XMgRNAQIn',
+	consumer_key =          lines[0],
+	consumer_secret =       lines[1],
+	access_token =          lines[2],
+	access_token_secret =   lines[3],
 )
+
