4907dd3170832b41d860db4c7a69b1bf3b4d1d89
Ubuntu <ubuntu@ip-172-31-25-173.us-east-2.compute.internal>
2017-07-19 17:42:24 +0000

    Updates to key

--- keys.py
+++ keys.py
@@ -1,14 +1,14 @@
 #So my private twitter auth keys aren't on git.....
-AllKeys = open('..\keys.txt', 'r')
+AllKeys = open('../keys.txt', 'r')
 lines = []
 for line in AllKeys:	
 	lines.append(line)
 AllKeys.close 
 
 keys = dict(
-	consumer_key =          lines[0],
-	consumer_secret =       lines[1],
-	access_token =          lines[2],
-	access_token_secret =   lines[3],
+	consumer_key = lines[0],
+	consumer_secret = lines[1],
+	access_token = lines[2],
+	access_token_secret = lines[3],
 )
 
